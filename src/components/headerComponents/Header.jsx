cd70b9333a033a066417323cdadaed6a9c4be577
JosefinaMor <josefina.morales.1765@gmail.com>
2022-09-17 16:59:53 -0300

    I resolve the menu´s problem

--- src/components/headerComponents/Header.jsx
+++ src/components/headerComponents/Header.jsx
@@ -1,20 +1,43 @@
 import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
 import Menu from './Menu';
 import NavBar from "./NavBar";
 
 const Header = () =>{
-  const [openMenu, setOpenMenu] = useState(false);
+  const [anchor, setAnchor] = useState("left");
+  const [state, setState] = useState({
+    left: false,
+  });
 
-  const openOrCloseSideMenu = (param) =>{
-    param ? setOpenMenu(false) : setOpenMenu(true);
-  }
+  const toggleDrawer = (anchor, open) => (event) => {
+    console.log(anchor)
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    console.log(anchor)
+    setState({ ...state, [anchor]: open });
+  };
 
   return(
     <>
       <NavBar 
-        onClick={()=>openOrCloseSideMenu(openMenu)}
+        onClick={toggleDrawer(anchor, true)}
       />
-      {openMenu && <Menu />}
+      <Drawer
+        anchor={anchor}
+        open={state[anchor]}
+        onClose={toggleDrawer(anchor, false)}
+      > 
+        <Box
+          sx={{ width:250 }}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        >
+          <Menu /> 
+        </Box>
+      </Drawer>
     </>
   )
 }
