0044145ccb44df849723071822de6de4991efc54
JosefinaMor <josefina.morales.1765@gmail.com>
2022-09-17 13:25:49 -0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+
+import AppBar from '@mui/material/AppBar';
+import Stack from '@mui/material/Stack';
+
+const NavBar = () =>{
+    return(
+        <>
+            <AppBar position="static">
+                <p>hola</p>
+            </AppBar>
+        </>
+    )
+}
+
+export default NavBar;
